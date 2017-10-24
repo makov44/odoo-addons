97d6083e4b7468d96d7352805da324b55940d61b
Sergiy Makovskyy <smakovskyy@dyl.com>
2017-10-24 13:24:35 -0700

    Updated audit_model and view

--- audit_ssh_keys/models/audit_models.py
+++ audit_ssh_keys/models/audit_models.py
@@ -11,9 +11,9 @@ class Host(models.Model):
     environment = fields.Char()
     description = fields.Text(string='Description')
     users_nbr = fields.Char(compute='_get_users', string='Number of Users')
-    user_ids = fields.One2many('audit_ssh_keys.user', 'host_id', string='Users')
+    users_ids = fields.One2many('audit_ssh_keys.user', 'host_id', string='Users')
 
-    # Compute number of contacts non opt-out for a mailing list
+    # Compute number of users
     def _get_users(self):
         self.env.cr.execute('''
                select
@@ -36,9 +36,23 @@ class User(models.Model):
     uri = fields.Char()
     description = fields.Text(string='Description')
     host_id = fields.Many2one('audit_ssh_keys.host', string='Host')
+    keys_nbr = fields.Char(compute='_get_keys', string='Number of SSH Keys')
+    keys_ids = fields.One2many('audit_ssh_keys.key', 'user_id', string='SSH keys')
 
-    keys_ids = fields.Many2many('audit_ssh_keys.key', 'audit_ssh_keys_user_key_rel',
-                                  'user_id', 'key_id', string='SSH keys')
+    # Compute number of keys
+    def _get_keys(self):
+        self.env.cr.execute('''
+                   select
+                       u.id, count(*)
+                   from
+                       audit_ssh_keys_user u
+                       left join audit_ssh_keys_key k on (k.user_id=u.id)              
+                   group by
+                       u.id
+               ''')
+        data = dict(self.env.cr.fetchall())
+        for user in self:
+            user.keys_nbr = data.get(user.id, 0)
 
 
 class SshKey(models.Model):
@@ -49,5 +63,4 @@ class SshKey(models.Model):
     key_hash = fields.Text()
     uri = fields.Char()
     description = fields.Text(string='Description')
-    user_ids = fields.Many2many('audit_ssh_keys.user', 'audit_ssh_keys_user_key_rel',
-                                'user_id', 'key_id', string='Users')
+    user_id = fields.Many2one('audit_ssh_keys.user', string="User")
