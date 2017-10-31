df8a3abb7560abb979abc9bf69cdc4449a076b73
Sergiy Makovskyy <smakovskyy@dyl.com>
2017-10-31 12:15:40 -0700

    Updated model and view to read from external datasource. Added breadcrumbs navigation.

--- audit_ssh_keys/models/audit_models.py
+++ audit_ssh_keys/models/audit_models.py
@@ -9,43 +9,26 @@ Query = rdf_manager.Query
 class Host(models.Model):
     _name = 'audit_ssh_keys.host'
 
-    host = fields.Char(string='Host Name')
-    uri = fields.Char()
+    name = fields.Char(string='Host Name')
     environment = fields.Char()
     description = fields.Text(string='Description')
-    users_nbr = fields.Char()
     users_ids = fields.One2many('audit_ssh_keys.user', 'host_id', string='Users')
-    _rdf_store = None
 
-    @api.multi
-    def write(self, vals):
-        pass
+    # @api.multi
+    # def write(self, vals):
+    #     pass
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
-        result = super().read(fields=None, load='_classic_read')
-        id = self.ids[0]
-        return RDF_STORE.execute(Query.host % (id))
-
-    # @api.model
-    # def search(self, args, offset=0, limit=None, order=None, count=True):
-    #     return [item["id"] for item in RDF_STORE.execute(RDF_STORE.host_count_query % (limit, offset))]
+        str_ids = '('+''.join([str(item)+',' for item in self.ids])+')'
+        return RDF_STORE.execute(Query.host % (str.rstrip(str_ids, ',)') + ')'))
 
     @api.model
-    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        """
-        Performs a ``search()`` followed by a ``read()``.
+    def search(self, args, offset=0, limit=10000, order=None, count=False):
+        return RDF_STORE.execute(Query.hosts % (limit, offset))
 
-        :param domain: Search domain, see ``args`` parameter in ``search()``. Defaults to an empty domain that will match all records.
-        :param fields: List of fields to read, see ``fields`` parameter in ``read()``. Defaults to all fields.
-        :param offset: Number of records to skip, see ``offset`` parameter in ``search()``. Defaults to 0.
-        :param limit: Maximum number of records to return, see ``limit`` parameter in ``search()``. Defaults to no limit.
-        :param order: Columns to sort result, see ``order`` parameter in ``search()``. Defaults to no sort.
-        :return: List of dictionaries containing the asked fields.
-        :rtype: List of dictionaries.
-
-        """
-
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=10000, order=None):
         return RDF_STORE.execute(Query.hosts % (limit, offset))
 
 
@@ -53,21 +36,66 @@ class User(models.Model):
     _name = 'audit_ssh_keys.user'
 
     name = fields.Char()
-    uri = fields.Char()
     description = fields.Text(string='Description')
     host_id = fields.Many2one('audit_ssh_keys.host', string='Host')
-    keys_nbr = fields.Char()
     keys_ids = fields.One2many('audit_ssh_keys.key', 'user_id', string='SSH keys')
 
+    @api.multi
+    def read(self, fields=None, load='_classic_read'):
+        str_ids = '(' + ''.join([str(item) + ',' for item in self.ids]) + ')'
+        return RDF_STORE.execute(Query.user % (str.rstrip(str_ids, ',)') + ')'))
 
+    @api.model
+    def search(self, args, offset=0, limit=10000, order=None, count=False):
+        return RDF_STORE.execute(Query.users % (limit, offset))
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=10000, order=None):
+        return RDF_STORE.execute(Query.users % (limit, offset))
+
+    @api.multi
+    def view(self):
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'audit_ssh_keys.user',
+            'res_id': self.id,
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+            'flags': {'form': {'action_buttons': True}}
+        }
 
 
 class SshKey(models.Model):
     _name = 'audit_ssh_keys.key'
 
-    label = fields.Char(string="Label")
+    name = fields.Char(string="Label")
     key_type = fields.Char(string='Type')
     key_hash = fields.Text()
-    uri = fields.Char()
     description = fields.Text(string='Description')
     user_id = fields.Many2one('audit_ssh_keys.user', string="User")
+
+    @api.multi
+    def read(self, fields=None, load='_classic_read'):
+        str_ids = '(' + ''.join([str(item) + ',' for item in self.ids]) + ')'
+        return RDF_STORE.execute(Query.key % (str.rstrip(str_ids, ',)') + ')'))
+
+    @api.model
+    def search(self, args, offset=0, limit=10000, order=None, count=False):
+        return RDF_STORE.execute(Query.keys % (limit, offset))
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=10000, order=None):
+        return RDF_STORE.execute(Query.keys % (limit, offset))
+
+    @api.multi
+    def view(self):
+        return {
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'audit_ssh_keys.key',
+            'res_id': self.id,
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+            'flags': {'form': {'action_buttons': True}}
+        }
--- audit_ssh_keys/models/rdf_manager.py
+++ audit_ssh_keys/models/rdf_manager.py
@@ -1,6 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-import hashlib
+import json
 from datetime import datetime
+import re
 
 
 class RdfStore:
@@ -20,7 +21,18 @@ class RdfStore:
                            'write_uid': (1, 'Administrator'),
                            'write_date': str(datetime.now())}
             for key in item:
-                dict_result[key] = item[key]["value"]
+                if key == 'name':
+                    dict_result["display_name"] = item[key]["value"]
+
+                if key == 'id':
+                    dict_result[key] = int(item[key]["value"])
+                elif '_json' in key:
+                    new_key = re.sub('_json$', '', key)
+                    dict_result[new_key] = json.loads(item[key]["value"])
+                else:
+                    dict_result[key] = item[key]["value"]
+
+
 
             query_result.append(dict_result)
 
@@ -31,10 +43,10 @@ class Query:
 
     hosts = """
             PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
-            SELECT DISTINCT ?id  ?uri  ?host 
+            SELECT DISTINCT ?id  ?name 
             WHERE { 
-            ?uri a ns1:system_instance;
-                     ns1:system_instance.hostname ?host;
+            ?hostUri a ns1:system_instance;
+                     ns1:system_instance.hostname ?name;
                      ns1:system_instance.id ?id . 
             }
             ORDER BY (?id)
@@ -44,36 +56,96 @@ class Query:
 
     host = """
                PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
-               SELECT DISTINCT ?id ?uri  ?host 
+               SELECT DISTINCT ?id  ?name  (concat('[' , group_concat(distinct ?userId; separator=','), ']') as ?users_ids_json)
                WHERE { 
-               ?uri a ns1:system_instance;
-                        ns1:system_instance.id %s .
+                   ?hostUri a ns1:system_instance;
+                        ns1:system_instance.id ?id;
+                        ns1:system_instance.hostname ?name .                       
+                   ?userUri a ns1:user;
+                         ns1:user.system ?hostUri;
+                         ns1:system_instance.id  ?userId .
+                   FILTER(?id in %s)              
                }
+               GROUP BY ?id ?uri ?name
                                             
                """
 
+    user = """
+              PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
+              SELECT DISTINCT ?id  ?name  ?host_id (concat('[' , group_concat(distinct ?key_id; separator=','), ']') as ?keys_ids_json)
+              WHERE { 
+                  ?hostUri a ns1:system_instance;
+                       ns1:system_instance.id ?host_id .  
+                  ?userUri a ns1:user;
+                       ns1:system_instance.id ?id;
+                       ns1:user.system ?hostUri;
+                       ns1:user.login  ?name .
+                  ?keyUri a ns1:ssh_key;
+                       ns1:system_instance.id ?key_id;
+                       ns1:ssh_key.key_type ?keyType;
+                       ns1:ssh_key.public_sha1 ?keyHash .
+                  ?authUri a ns1:authorized_key;
+                       ns1:authorized_key.user  ?userUri;
+                       ns1:authorized_key.ssh_key ?keyUri;
+                       ns1:authorized_key.label ?label  .                         
+                  
+                  FILTER(?id in %s)              
+              }
+              GROUP BY ?id  ?name  ?host_id
+
+              """
+
     users = """
                 PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
-                SELECT DISTINCT ?uri  ?user  ?hostUri
+                SELECT DISTINCT ?id  ?name  ?host_id
                 WHERE {
-                ?hostUri a ns1:system_instance .
-                ?uri a ns1:user;
-                     ns1:user.login ?user;
-                     ns1:user.system ?hostUri .
+                    ?hostUri a ns1:system_instance;
+                            ns1:system_instance.id  ?host_id .
+                    ?userUri a ns1:user;
+                         ns1:user.login ?user;
+                         ns1:user.system ?hostUri;
+                         ns1:system_instance.id  ?id .
+                 }
+                 ORDER BY (?id)
+                 LIMIT %s
+                 OFFSET %s
+                """
+
+    key = """
+                PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
+                SELECT DISTINCT ?id ?user_id ?name ?key_hash ?key_type
+                WHERE { 
+                    ?userUri a ns1:user;
+                            ns1:system_instance.id  ?user_id .                       
+                    ?keyUri a ns1:ssh_key;
+                            ns1:ssh_key.key_type ?key_type;
+                            ns1:ssh_key.public_sha1 ?key_hash; 
+                            ns1:system_instance.id  ?id .
+                    ?authUri a ns1:authorized_key;
+                            ns1:authorized_key.user  ?userUri;
+                            ns1:authorized_key.ssh_key ?keyUri;
+                            ns1:authorized_key.label ?name  . 
+                    FILTER(?id in %s)                        
                 }
+                ORDER BY (?id)               
                 """
 
     keys = """
                 PREFIX ns1: <http://rdf.siliconbeach.io/schema/sys/v1/>
-                SELECT DISTINCT ?uri ?label ?keyHash ?keyType  ?userUri
+                SELECT DISTINCT ?id ?user_id ?name ?key_hash ?key_type
                 WHERE { 
-                ?userUri a ns1:user .                       
-                ?uri a ns1:ssh_key;
-                        ns1:ssh_key.key_type ?keyType;
-                        ns1:ssh_key.public_sha1 ?keyHash .
-                ?authUri a ns1:authorized_key;
-                        ns1:authorized_key.user  ?userUri;
-                        ns1:authorized_key.ssh_key ?uri;
-                        ns1:authorized_key.label ?label  .                      
+                    ?userUri a ns1:user:
+                            ns1:system_instance.id  ?user_id .                        
+                    ?keyUri a ns1:ssh_key;
+                            ns1:ssh_key.key_type ?key_type;
+                            ns1:ssh_key.public_sha1 ?key_hash; 
+                            ns1:system_instance.id  ?id .
+                    ?authUri a ns1:authorized_key;
+                            ns1:authorized_key.user  ?userUri;
+                            ns1:authorized_key.ssh_key ?keyUri;
+                            ns1:authorized_key.label ?name  .                      
                 }
+                ORDER BY (?id)
+                LIMIT %s
+                OFFSET %s
                 """
